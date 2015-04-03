dae6d5c29f0c6ec4c8e586c840ab9cec842273aa
Allan Renucci <allan.renucci@gmail.com>
2015-04-03 17:58:57 +0200

    Fix indentation

--- src/main/scala/ch/epfl/structure/StructureParser.scala
+++ src/main/scala/ch/epfl/structure/StructureParser.scala
@@ -21,9 +21,9 @@ object StructureParser {
 
   private implicit val paramsReads: Reads[Params] = (
     (JsPath \ "A~A").read[Param] and
-      (JsPath \ "B~B").read[Param] and
-      (JsPath \ "A~B").read[Param]
-    )(Params.apply _)
+    (JsPath \ "B~B").read[Param] and
+    (JsPath \ "A~B").read[Param]
+  )(Params.apply _)
 
   private implicit val speciesReads = Json.reads[Species]
 
@@ -37,25 +37,25 @@ object StructureParser {
 
   private implicit val spaceGroupReads: Reads[SpaceGroup] = (
     (JsPath \ "point_group"   ).read[String] and
-      (JsPath \ "source"        ).read[String] and
-      (JsPath \ "crystal_system").read[String] and
-      (JsPath \ "hall"          ).read[String] and
-      (JsPath \ "symbol"        ).read[String] and
-      (JsPath \ "number"        ).read[Double]
-    )(SpaceGroup.apply _)
+    (JsPath \ "source"        ).read[String] and
+    (JsPath \ "crystal_system").read[String] and
+    (JsPath \ "hall"          ).read[String] and
+    (JsPath \ "symbol"        ).read[String] and
+    (JsPath \ "number"        ).read[Double]
+  )(SpaceGroup.apply _)
 
   private implicit val structureReads: Reads[Structure] = (
     (JsPath \ "_id" \ "$oid"        ).read[String] and
-      (JsPath \ "elements"            ).read[Seq[String]] and
-      (JsPath \ "energy"              ).read[Double] and
-      (JsPath \ "pressure"            ).read[Double] and
-      (JsPath \ "spacegroup"          ).read[SpaceGroup] and
-      (JsPath \ "unit_cell_formula"   ).read[Map[String, Double]] and
-      (JsPath \ "structure"           ).read[Struct] and
-      (JsPath \ "reduced_cell_formula").read[Map[String, Double]] and
-      (JsPath \ "nelements"           ).read[Double] and
-      (JsPath \ "nsites"              ).read[Double] and
-      (JsPath \ "chemsys"             ).read[String] and
-      (JsPath \ "potential"           ).read[Potential]
-    )(Structure.apply _)
+    (JsPath \ "elements"            ).read[Seq[String]] and
+    (JsPath \ "energy"              ).read[Double] and
+    (JsPath \ "pressure"            ).read[Double] and
+    (JsPath \ "spacegroup"          ).read[SpaceGroup] and
+    (JsPath \ "unit_cell_formula"   ).read[Map[String, Double]] and
+    (JsPath \ "structure"           ).read[Struct] and
+    (JsPath \ "reduced_cell_formula").read[Map[String, Double]] and
+    (JsPath \ "nelements"           ).read[Double] and
+    (JsPath \ "nsites"              ).read[Double] and
+    (JsPath \ "chemsys"             ).read[String] and
+    (JsPath \ "potential"           ).read[Potential]
+  )(Structure.apply _)
 }
