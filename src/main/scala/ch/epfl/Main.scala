b72e37a86e3310be7365ffd77b60464dc553c5f0
Allan Renucci <allan.renucci@gmail.com>
2015-04-14 15:44:59 +0200

    Updata main for cluster

--- src/main/scala/ch/epfl/Main.scala
+++ src/main/scala/ch/epfl/Main.scala
@@ -1,15 +1,24 @@
 package ch.epfl
 
 import ch.epfl.structure.StructureParser
+import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.io.Source
 
 object Main {
   def main(args: Array[String]) {
-    val fileName = getClass getResource "/structures.json"
-    val source = Source fromURL fileName
+    // val fileName = getClass getResource "/structures.json"
+    // val source = Source fromURL fileName
 
-    val structs = source getLines() flatMap StructureParser.parse
+    // val structs = source getLines() flatMap StructureParser.parse
+    // structs foreach println
+
+    val fileName = "/projects/aiida/structures.json"
+    val conf = new SparkConf() setAppName "Structure Analysis"
+    val sc = new SparkContext(conf)
+
+    val lines = sc textFile fileName
+    val structs = lines flatMap StructureParser.parse
     structs foreach println
   }
 }
