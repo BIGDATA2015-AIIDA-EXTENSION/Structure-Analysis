5a636e519d1107b06373997d37fcb5f87bcb2173
LukasKellenberger <lukas.kellenberger@gmail.com>
2015-04-20 12:02:43 +0200

    Groups records and takes min energy

--- src/main/scala/ch/epfl/computations/AiidaComputations.scala
+++ src/main/scala/ch/epfl/computations/AiidaComputations.scala
@@ -1,6 +1,6 @@
 package ch.epfl.computations
 
-import ch.epfl.structure.{Params, Structure, Struct, StructureParser}
+import ch.epfl.structure.{Params, Structure, StructureParser}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -15,32 +15,14 @@ object AiidaComputations {
 
     parsed.cache()
 
-    val mapViewer = new MapViewer(List(AAEpsilon(1), ABEpsilon(1.5)))
+    val mapViewer = new MapViewer(List(AAEpsilon(1), ABEpsilon(1.5)), ABSigma, BBEpsilon)
 
-    val filteredSorted = mapViewer.getSortedResults(parsed)
+    val groups = mapViewer.getMap(parsed)
 
-    println("######## "+filteredSorted.count())
-
-    filteredSorted.saveAsTextFile("hdfs:///user/lukas/project/output/")
+    groups.saveAsTextFile("hdfs://" + args(1))
     sc.stop()
   }
 
-  def distance(xyz1: Seq[Double], xyz2: Seq[Double]): Double = {
-    // Pairs the coordinates axis by axis
-    val pairs = xyz1.zip(xyz2)
-    // Sums the squares of the difference of the coordinates (which is the squared distance)
-    val sumOfSquares = pairs.foldLeft(0.0)((acc, cpl) => acc + (cpl._1 - cpl._2) * (cpl._1 - cpl._2))
-    // Actual distance
-    Math.sqrt(sumOfSquares)
-  }
-
-  def areSimilar(struct1: Struct, struct2: Struct, errorMargin: Double): Boolean = {
-    // We assume that they have the same unitCellFormula
-    val sites1 = struct1.sites
-    val sites2 = struct2.sites
-
-    sites1.forall(elm1 => sites2.exists(elm2 => distance(elm1.xyz, elm2.xyz) < errorMargin))
-  }
 }
 
 trait Condition {
@@ -67,14 +49,47 @@ case class ABEpsilon(value: Double) extends Condition {
 }
 
 
-class MapViewer(conds: List[Condition]) {
-  def getSortedResults(rdd: RDD[Structure]): RDD[Double] = {
+trait MapAxis {
+  def getValue(structure: Structure): Double
+}
+case object AASigma extends MapAxis {
+  override def getValue(structure: Structure): Double = structure.potential.params.aa.sigma
+}
+case object AAEpsilon extends MapAxis {
+  override def getValue(structure: Structure): Double = structure.potential.params.aa.epsilon
+}
+case object ABSigma extends MapAxis {
+  override def getValue(structure: Structure): Double = structure.potential.params.ab.sigma
+}
+case object ABEpsilon extends MapAxis {
+  override def getValue(structure: Structure): Double = structure.potential.params.ab.epsilon
+}
+case object BBSigma extends MapAxis {
+  override def getValue(structure: Structure): Double = structure.potential.params.bb.sigma
+}
+case object BBEpsilon extends MapAxis {
+  override def getValue(structure: Structure): Double = structure.potential.params.bb.epsilon
+}
+
+
+
+
+case class MapViewer(conds: List[Condition], mapAxisX: MapAxis, mapAxisY: MapAxis) {
+  private def getFilteredResults(rdd: RDD[Structure]): RDD[Structure] = {
 
     def applyCond(conds: List[Condition], rdd: RDD[Structure]): RDD[Structure] = conds match {
       case Nil => rdd
       case cond :: tail => applyCond(tail, rdd.filter(s => cond.condition(s.potential.params)))
     }
 
-    applyCond(conds, rdd).sortBy(s => s.energy).map(s => s.energy)
+    applyCond(conds, rdd)
   }
+
+  def getMap(rdd: RDD[Structure]): RDD[((Double, Double), Structure)] = {
+
+    def groupCond(structure: Structure) = (mapAxisX.getValue(structure), mapAxisY.getValue(structure))
+
+    getFilteredResults(rdd).groupBy(groupCond).map{ case (key, value) => (key, value.minBy(_.energyPerSite)) }
+  }
+
 }
