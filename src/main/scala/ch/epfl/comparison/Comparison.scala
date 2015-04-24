1ff0bc287bd924d8c573619dd5b11b09639592d5
Martin Duhem <martin@c-webhosting.org>
2015-04-24 18:12:01 +0200

    Finish structure comparator

--- src/main/scala/ch/epfl/comparison/Comparison.scala
+++ src/main/scala/ch/epfl/comparison/Comparison.scala
@@ -25,37 +25,57 @@ object Comparator {
     val aCrossCLen = Math.sqrt(aCrossC dot aCrossC)
     val aCrossCHat = aCrossC / aCrossCLen;
 
-    val cutoff = max(a.norm, b.norm, c.norm)
+    val cutoff = cutoffFactor * max(a.norm, b.norm, c.norm)
   }
 
-  def distance(s1: Structure, s2: Structure): Double = {
-    val unitCell1 = lattice2UnitCell(s1.struct.lattice)
-    val ds1 = distances(s1, unitCell = s1.struct.lattice, maxCellMultiples = maxCellMultiples, cutoff = unitCell1.cutoff, maxValues = maxValues)
+  def distance(s1: Structure, s2: Structure): Option[Double] = {
 
-    val unitCell2 = lattice2UnitCell(s2.struct.lattice)
-    val ds2 = distances(s2, unitCell = s2.struct.lattice, maxCellMultiples = maxCellMultiples, cutoff = unitCell2.cutoff, maxValues = maxValues)
+    if (s1.nbElements != s2.nbElements) None
+    else if ((s1.elements.toSet intersect s2.elements.toSet).isEmpty) None
+    else {
+      val ds1 = distances(s1)
+      val ds2 = distances(s2)
 
-    // Not sure how to compute the difference between two set of list.
-    // What order should be used, what about set/list of different size...
-    val diff = ???
+      val sum =
+        ds1 map {
+          case (k, _vs1) =>
+            val _vs2 = ds2(k)
+            val min = _vs1.size min _vs2.size
 
-    // TODO Compute the root mean square of the diff
-    ???
+            val vs1 = _vs1 take min
+            val vs2 = _vs2 take min
+
+            ((vs1 zip vs2) foldLeft 0.0) {
+              case (acc, (d1, d2)) =>
+                val sum = d1 + d2
+                val delta = if (sum > 0) Math.abs(d1 - d2) / sum else 0
+                delta * delta
+            }
+        }
+
+      Some(Math.sqrt(sum.sum))
+    }
   }
 
   /**
    * Given a structure return a Map from a pair of atoms to a sorted sequence of distances
    */
-  def distances(s: Structure, unitCell: UnitCell, maxCellMultiples: Int, cutoff: Double, maxValues: Int): Map[(Site, Site), Seq[Double]] = {
-    val combinations = s.struct.sites combinations 2
-    val dists = combinations map { case Seq(i, j) => (i, j) -> distances(i, j, unitCell, maxCellMultiples, cutoff, maxValues).sorted }
-    dists.toMap
+  def distances(s: Structure): Map[(Species, Species), Seq[Double]] = {
+
+    val unitCell = lattice2UnitCell(s.struct.lattice)
+    val combinations = (s.struct.sites combinations 2).toSeq
+
+    combinations flatMap {
+      case Seq(i, j) => distances(i, j, unitCell)
+    } groupBy (_._1) map { case (k, v) => (k, v.map(_._2).flatten.sorted) }
+
   }
 
   /**
    * Compute the distances between two atoms
    */
-  def distances(s1: Site, s2: Site, unitCell: UnitCell, maxCellMultiples: Int, cutoff: Double, maxValues: Int): Seq[Double] = {
+  def distances(s1: Site, s2: Site, unitCell: UnitCell): Seq[((Species, Species), Seq[Double])] = {
+    val cutoff = unitCell.cutoff
     val a = DenseVector(s1.abc.toArray)
     val b = DenseVector(s2.abc.toArray)
 
@@ -68,22 +88,30 @@ object Comparator {
 
     val cutoffSq = cutoff * cutoff
 
-    val results =
-      for {
-        a <- -aMax to aMax
-        rA = a.toDouble * unitCell.a
-        b <- -bMax to bMax
-        rAB = rA + b.toDouble * unitCell.b
-        c <- -cMax to cMax
-        outVec = rAB + c.toDouble * unitCell.c + dR
-        testDistSq = outVec dot outVec
-        testDist = Math.sqrt(testDistSq) if testDistSq < cutoffSq
-      } yield {
-        testDist
-      }
+    val results = {
+      val tmpResults =
+        for {
+            a <- (-aMax to aMax).toStream
+            rA = a.toDouble * unitCell.a
+            b <- -bMax to bMax
+            rAB = rA + b.toDouble * unitCell.b
+            c <- -cMax to cMax
+            outVec = rAB + c.toDouble * unitCell.c + dR
+            testDistSq = outVec dot outVec
+            testDist = Math.sqrt(testDistSq) if testDistSq < cutoffSq
+          } yield {
+            testDist
+          }
 
-    if (results.length < maxValues) results
-    else Seq()
+      (tmpResults take maxValues).toSeq
+    }
+
+    for {
+      spec1 <- s1.species
+      spec2 <- s2.species if spec1.element <= spec2.element
+    } yield (spec1, spec2) -> results
+
+
   }
 
   def getNumPlaneRepetitionsToBoundSphere(radius: Double, volume: Double, crossLen: Double): Double = {
